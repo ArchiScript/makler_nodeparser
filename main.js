05b706f545d6b8d7b19f6108dd87466161127201
Artur Skomarohov <arturskomarohov@gmail.com>
2025-08-12 16:48:30 +0300

    fix(main)[build]: adjust changed port in code base

--- main.js
+++ main.js
@@ -171,7 +171,7 @@ async function connectToBrowserless(retries = 5, delayMs = 2000) {
 
 // Express server setup
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 35001;
 
 
 //scraper endpoint
